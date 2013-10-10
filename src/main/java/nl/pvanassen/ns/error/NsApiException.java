7015c39b66d05519f8683b93a80031e32ee0a506
Paul van Assen <source@paules.nl>
2013-10-10 21:47:00 +0200

    Ns api Exception

---
+++ src/main/java/nl/pvanassen/ns/error/NsApiException.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package nl.pvanassen.ns.error;
+
+/**
+ * Exception thrown when the API fails
+ * @author Paul van Assen
+ *
+ */
+public class NsApiException extends RuntimeException {
+
+	private static final long serialVersionUID = -4535400016864458414L;
+
+	/**
+	 * @param message Error message
+	 */
+	public NsApiException(String message) {
+		super(message);
+	}
+
+	/**
+	 * @param message Error message
+	 * @param cause Error cause
+	 */
+	public NsApiException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
