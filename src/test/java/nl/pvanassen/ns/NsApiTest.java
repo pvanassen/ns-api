775202005d560e76086c324001de90b3fa193113
Paul van Assen <source@paules.nl>
2013-12-27 16:50:53 +0100

    Added test for ns api http logic and input validation

---
+++ src/test/java/nl/pvanassen/ns/NsApiTest.java
@@ -0,0 +1,45 @@
+package nl.pvanassen.ns;
+
+import static org.junit.Assert.*;
+import nl.pvanassen.ns.error.NsApiException;
+
+import org.junit.Test;
+
+
+public class NsApiTest {
+
+    @Test(expected=NullPointerException.class)
+    public void testNsApiNull() {
+        new NsApi(null, null);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testNsApiEmpty() {
+        new NsApi("", "");
+    }
+
+    @Test(expected=NsApiException.class)
+    public void testGetApiResponseString() {
+        NsApi nsApi = new NsApi("test", "invalid");
+        nsApi.getApiResponse(new ApiRequest<String>() {
+            @Override
+            String getPath() {
+                return "";
+            }
+            @Override
+            String getRequestString() {
+                return "";
+            }
+            @Override
+            Class<String> getType() {
+                return String.class;
+            }
+        });
+    }
+
+    @Test(expected=NsApiException.class)
+    public void testGetApiResponseStations() {
+        NsApi nsApi = new NsApi("test", "invalid");
+        nsApi.getApiResponse(new StationsRequest());
+    }
+}
