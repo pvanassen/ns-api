118887a988561328b65166ea50e8ceae911c1fcd
Paul van Assen <source@paules.nl>
2013-12-27 16:06:34 +0100

    Hiding constructor of utility class

--- src/main/java/nl/pvanassen/ns/RequestBuilder.java
+++ src/main/java/nl/pvanassen/ns/RequestBuilder.java
@@ -4,19 +4,26 @@ import nl.pvanassen.ns.model.stations.Stations;
 import nl.pvanassen.ns.model.storingen.Storingen;
 import nl.pvanassen.ns.model.vertrektijden.ActueleVertrekTijden;
 
-
 public class RequestBuilder {
+
     // Only one instance possible. Pre-instantiating this
     private static final StationsRequest INSTANCE = new StationsRequest();
+
+    /**
+     * Hiding utility class constructor
+     */
+    private RequestBuilder() {
+        super();
+    }
 
     public static ApiRequest<ActueleVertrekTijden> getActueleVertrektijden(String station) {
         return new ActueleVertrekTijdenRequest(station);
     }
-    
+
     public static ApiRequest<Stations> getStations() {
         return INSTANCE;
     }
-    
+
     /**
      * Builds a request to return all 'Actuele storingen'
      * 
@@ -28,19 +35,19 @@ public class RequestBuilder {
 
     /**
      * Builds a request that will return all 'Geplande werkzaamheden'
-
+     * 
      * @return All 'Geplanden werkzaamheden' request
      */
-    public static  ApiRequest<Storingen> getGeplandeWerkzaamhedenRequest() {
+    public static ApiRequest<Storingen> getGeplandeWerkzaamhedenRequest() {
         return new StoringenEnWerkzaamhedenRequest(null, null, Boolean.TRUE);
     }
-    
+
     /**
      * Builds a request that will return all 'Geplande werkzaamheden'
-
+     * 
      * @return All 'Geplanden werkzaamheden' request
      */
-    public static  ApiRequest<Storingen> getActueleStoringen(String station) {
+    public static ApiRequest<Storingen> getActueleStoringen(String station) {
         return new StoringenEnWerkzaamhedenRequest(station, null, null);
     }
 
