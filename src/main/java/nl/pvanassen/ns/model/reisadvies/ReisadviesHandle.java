0d5ed16e949df99af309c5bfb414753d3eb3e98f
Paul van Assen <source@paules.nl>
2013-12-31 12:23:11 +0100

    Found possibility more than one 'Melding' can be in the XML. Changed handle, object and test to accomodate this

--- src/main/java/nl/pvanassen/ns/model/reisadvies/ReisadviesHandle.java
+++ src/main/java/nl/pvanassen/ns/model/reisadvies/ReisadviesHandle.java
@@ -3,9 +3,7 @@ package nl.pvanassen.ns.model.reisadvies;
 import java.io.InputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import nl.pvanassen.ns.NsApi;
 import nl.pvanassen.ns.error.NsApiException;
@@ -15,10 +13,22 @@ import nl.pvanassen.ns.xml.Xml;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Handle to process the 'reisadvies' XML as defined in <a
+ * href="http://www.ns.nl/api/api#api-documentatie-reisadviezen">documentatie reisadviezen</a>
+ * 
+ * @author Paul van Assen
+ * 
+ */
 public class ReisadviesHandle implements Handle<ReisMogelijkheden> {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @see nl.pvanassen.ns.handle.Handle#getModel(java.io.InputStream)
+     */
     @Override
     public ReisMogelijkheden getModel(InputStream stream) {
         SimpleDateFormat format = new SimpleDateFormat(NsApi.DATETIME_FORMAT);
@@ -26,29 +36,30 @@ public class ReisadviesHandle implements Handle<ReisMogelijkheden> {
             Xml xml = Xml.getXml(stream, "ReisMogelijkheden");
             List<ReisMogelijkheid> reisMogelijkheden = new ArrayList<ReisMogelijkheid>(xml.children("ReisMogelijkheid")
                     .size());
-            for (Xml reisMovelijkheidXml : xml.children("ReisMogelijkheid")) {
-                Melding melding = null;
-                if (reisMovelijkheidXml.isPresent("Melding")) {
-                    Xml meldingXml = reisMovelijkheidXml.child("Melding");
-                    String id = meldingXml.child("Id").content();
-                    boolean ernstig = Boolean.parseBoolean(meldingXml.child("Ernstig").content());
-                    String text = meldingXml.child("Text").content();
-                    melding = new Melding(id, ernstig, text);
+            for (Xml reisMogelijkheidXml : xml.children("ReisMogelijkheid")) {
+                List<Melding> meldingen = new LinkedList<Melding>();
+                if (reisMogelijkheidXml.isPresent("Melding")) {
+                    for (Xml meldingXml : reisMogelijkheidXml.children("Melding")) {
+                        String id = meldingXml.child("Id").content();
+                        boolean ernstig = Boolean.parseBoolean(meldingXml.child("Ernstig").content());
+                        String text = meldingXml.child("Text").content();
+                        meldingen.add(new Melding(id, ernstig, text));
+                    }
                 }
-                int aantalOverstappen = Integer.parseInt(reisMovelijkheidXml.child("AantalOverstappen").content());
-                int geplandeReisTijdMinuten = getReistijdInMinuten(reisMovelijkheidXml.child("GeplandeReisTijd")
+                int aantalOverstappen = Integer.parseInt(reisMogelijkheidXml.child("AantalOverstappen").content());
+                int geplandeReisTijdMinuten = getReistijdInMinuten(reisMogelijkheidXml.child("GeplandeReisTijd")
                         .content());
-                int actueleReisTijdMinuten = getReistijdInMinuten(reisMovelijkheidXml.child("ActueleReisTijd")
+                int actueleReisTijdMinuten = getReistijdInMinuten(reisMogelijkheidXml.child("ActueleReisTijd")
                         .content());
-                boolean optimaal = Boolean.parseBoolean(reisMovelijkheidXml.child("Optimaal").content());
-                Date geplandeVertrekTijd = format.parse(reisMovelijkheidXml.child("GeplandeVertrekTijd").content());
-                Date actueleVertrekTijd = format.parse(reisMovelijkheidXml.child("ActueleVertrekTijd").content());
-                Date geplandeAankomstTijd = format.parse(reisMovelijkheidXml.child("GeplandeAankomstTijd").content());
-                Date actueleAankomstTijd = format.parse(reisMovelijkheidXml.child("ActueleAankomstTijd").content());
-                String aankomstVertraging = reisMovelijkheidXml.child("AankomstVertraging").content();
-                String status = reisMovelijkheidXml.child("Status").content();
-                List<ReisDeel> reisDelen = new ArrayList<ReisDeel>(reisMovelijkheidXml.children("ReisDeel").size());
-                for (Xml reisDeelXml : reisMovelijkheidXml.children("ReisDeel")) {
+                boolean optimaal = Boolean.parseBoolean(reisMogelijkheidXml.child("Optimaal").content());
+                Date geplandeVertrekTijd = format.parse(reisMogelijkheidXml.child("GeplandeVertrekTijd").content());
+                Date actueleVertrekTijd = format.parse(reisMogelijkheidXml.child("ActueleVertrekTijd").content());
+                Date geplandeAankomstTijd = format.parse(reisMogelijkheidXml.child("GeplandeAankomstTijd").content());
+                Date actueleAankomstTijd = format.parse(reisMogelijkheidXml.child("ActueleAankomstTijd").content());
+                String aankomstVertraging = reisMogelijkheidXml.child("AankomstVertraging").content();
+                String status = reisMogelijkheidXml.child("Status").content();
+                List<ReisDeel> reisDelen = new ArrayList<ReisDeel>(reisMogelijkheidXml.children("ReisDeel").size());
+                for (Xml reisDeelXml : reisMogelijkheidXml.children("ReisDeel")) {
                     String reisSoort = reisDeelXml.attr("reisSoort");
                     String vervoerder = reisDeelXml.child("Vervoerder").content();
                     String vervoerType = reisDeelXml.child("VervoerType").content();
@@ -73,7 +84,7 @@ public class ReisadviesHandle implements Handle<ReisMogelijkheden> {
                     reisDelen.add(new ReisDeel(reisSoort, vervoerder, vervoerType, ritNummer, statusReisdeel,
                             reisStops, ongeplandeStoringId, geplandeStoringId, reisDetails));
                 }
-                reisMogelijkheden.add(new ReisMogelijkheid(melding, aantalOverstappen, geplandeReisTijdMinuten,
+                reisMogelijkheden.add(new ReisMogelijkheid(meldingen, aantalOverstappen, geplandeReisTijdMinuten,
                         actueleReisTijdMinuten, aankomstVertraging, optimaal, geplandeVertrekTijd, actueleVertrekTijd,
                         geplandeAankomstTijd, actueleAankomstTijd, status, reisDelen));
             }
